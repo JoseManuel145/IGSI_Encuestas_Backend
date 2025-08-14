a5372b54bf622c2f3033c19ddd664b8b82951e1e
JoseManuel145 <233328@ids.upchiapas.edu.mx>
2025-08-14 12:09:37 -0600

    Subiendo la Base del proyecto

---
+++ encuestas/src/test/java/com/igsi/encuestas/EncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igsi.encuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
